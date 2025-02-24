692cc1508fe36bed977d91d464f05fd8e6c240a6
kunakorn Mlthongfak <kunakorn Mlthongfak>
2025-02-24 20:42:35 +0700

    kunakorn Multhongfak

--- app/src/main/java/scisrc/mobiledev/ecommercelayout/ui/HomeFragment.kt
+++ app/src/main/java/scisrc/mobiledev/ecommercelayout/ui/HomeFragment.kt
@@ -1,30 +1,17 @@
 package scisrc.mobiledev.ecommercelayout.ui
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import scisrc.mobiledev.ecommercelayout.databinding.FragmentHomeBinding
+import scisrc.mobiledev.ecommercelayout.R
 
-class HomeFragment : Fragment() {
-
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
-
+class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // ONLY inflate layout here
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
-
-    // function to do the action in this fragment
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
 }
